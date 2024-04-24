ef3c371348faebb891db3ee08acde417df73351f
dawmsi <da._.off@outlook.com>
2024-04-24 12:19:06 +0300

    navigation in AppBar styling

--- src/features/Header/AppHeader.tsx
+++ src/features/Header/AppHeader.tsx
@@ -1,35 +1,8 @@
 import { AppBar, Box, Link, Toolbar, Typography } from '@mui/material';
 import LiveTvOutlinedIcon from '@mui/icons-material/LiveTvOutlined';
-import { Link as RouterLink } from 'react-router-dom';
+import { NavLink as RouterLink } from 'react-router-dom';
 import { AuthSection } from './AuthSection';
 import { useAuth0 } from '@auth0/auth0-react';
-
-export function AppHeader() {
-  const { isAuthenticated } = useAuth0();
-
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <LiveTvOutlinedIcon sx={{ mr: 2 }} />
-        <Typography variant="h6" color="inherit" noWrap>
-          The Movies DB
-        </Typography>
-        <Box sx={{ flexGrow: 1 }}>
-          <nav>
-            <HeaderLink to="/">Home</HeaderLink>
-            <HeaderLink to="/movies">Movies</HeaderLink>
-            <HeaderLink to="/extra">Extra</HeaderLink>
-            {isAuthenticated && (
-              <HeaderLink to="/protected">Protected</HeaderLink>
-            )}
-            <HeaderLink to="/about">About</HeaderLink>
-          </nav>
-        </Box>
-        <AuthSection />
-      </Toolbar>
-    </AppBar>
-  );
-}
 
 function HeaderLink({
   to,
@@ -44,8 +17,45 @@ function HeaderLink({
       to={to}
       variant="button"
       color="inherit"
-      sx={{ my: 1, mx: 1.5 }}>
+      sx={{ my: 1, mx: 1.5, textDecoration: 'none' }}>
       {children}
     </Link>
+  );
+}
+
+export function AppHeader() {
+  const { isAuthenticated } = useAuth0();
+
+  const navigation = [
+    { name: 'Home', path: '/' },
+    { name: 'Movies', path: '/movies' },
+    { name: 'Extra', path: '/extra' },
+    { name: 'Protected_Api', path: '/protected' },
+    { name: 'About', path: '/about' },
+  ];
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <LiveTvOutlinedIcon sx={{ mr: 2 }} />
+        <Typography variant="h6" color="inherit" noWrap>
+         MoviesTV
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+          <nav>
+            {navigation.map(({ name, path }, index) => {
+              if (path === '/protected' && !isAuthenticated) return;
+
+              return (
+                <HeaderLink key={index} to={path}>
+                  {name}
+                </HeaderLink>
+              );
+            })}
+          </nav>
+        </Box>
+        <AuthSection />
+      </Toolbar>
+    </AppBar>
   );
 }
