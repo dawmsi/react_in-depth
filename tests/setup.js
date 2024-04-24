e00755e5aacfce9491d1c425a54c2c8801719d89
dawmsi <da._.off@outlook.com>
2024-04-24 21:21:10 +0300

    corrent path

--- tests/setup.js
+++ tests/setup.js
@@ -2,7 +2,7 @@ import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
 
-import "./jest.polyfills";
+import "../jest.polyfills";
 
 expect.extend(matchers);
 
